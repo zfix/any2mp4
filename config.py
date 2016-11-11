bf8ffb095c4939b95e40365c7c9160268022f6c6
Oleg Tokar <zfix@zfix.pw>
2016-11-12 01:43:30 +0200

    Create README.md

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
+# few variants of encoders, uncomment one of this
 cmdtempl=['HandBrakeCLI', '-e', 'x264',  '-q', '20.0', '-a', '1,2,3,4',
           '-E', 'faac', '-B', '160', '-R', 'Auto', '-D', '0.0',
           '--audio-copy-mask', 'aac,ac3,dtshd,dts,mp3', '--audio-fallback', 'ffac3',
@@ -9,10 +10,15 @@ cmdtempl=['HandBrakeCLI', '-Z', 'Normal', '-i', '"SRC"', '-o', '"DST"']
 #           '2500k', '-preset', 'veryfast', '-acodec', 'libfdk_aac',
 #           '-ab', '128k', '-ac', '2', '-ar', '48k',
 #           '-map', '0:v', '-map', '0:a', '-f', 'mp4', '"DST"']
+# command for file checking
 cmdffp=['ffprobe', '-show_streams', '-of', 'json', '"SRC"']
+# input directory
 indir="/media/films/"
-logfile="/home/user/tr.log"
+# list of files in input directory
 srclist="/home/user/mkvfiles.txt"
+logfile="/home/user/tr.log"
+# extensions of source files
 inext="mkv"
+# regexp for renaming
 regexpsearch = '(.*)\.%s' % inext
 # regexpsearch = '(.*)\.(mkv|avi|mov|ts|mpg)'
