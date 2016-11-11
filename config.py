6265ffcb693b39692aeb103a38f5cc21d58b4230
Oleg Tokar <zfix@zfix.pw>
2016-11-12 01:14:09 +0200

    1st

---
+++ config.py
@@ -0,0 +1,18 @@
+cmdtempl=['HandBrakeCLI', '-e', 'x264',  '-q', '20.0', '-a', '1,2,3,4',
+          '-E', 'faac', '-B', '160', '-R', 'Auto', '-D', '0.0',
+          '--audio-copy-mask', 'aac,ac3,dtshd,dts,mp3', '--audio-fallback', 'ffac3',
+          '-f', 'mp4', '--loose-anamorphic', '--modulus', '2', '-m',
+          '--x264-preset', 'veryfast', '--h264-profile', 'main',
+          '--h264-level', '4.0', '-i', 'SRC', '-o', 'DST']
+cmdtempl=['HandBrakeCLI', '-Z', 'Normal', '-i', '"SRC"', '-o', '"DST"']
+# cmdtempl=['ffmpeg', '-i', '"SRC"', '-y', '-vcodec', 'libx264', '-b',
+#           '2500k', '-preset', 'veryfast', '-acodec', 'libfdk_aac',
+#           '-ab', '128k', '-ac', '2', '-ar', '48k',
+#           '-map', '0:v', '-map', '0:a', '-f', 'mp4', '"DST"']
+cmdffp=['ffprobe', '-show_streams', '-of', 'json', '"SRC"']
+indir="/media/films/stroage1/Multfilmy"
+logfile="/home/tos/tr.log"
+srclist="/home/tos/mkvfiles.txt"
+inext="mkv"
+regexpsearch = '(.*)\.%s' % inext
+# regexpsearch = '(.*)\.(mkv|avi|mov|ts|mpg)'
---
+++ main.py
@@ -0,0 +1,114 @@
+from config import *
+import subprocess as SP
+import re
+import logging
+import sys
+import os
+import json
+
+
+def newname(oldname):
+    newfile = re.findall(regexpsearch, oldname)
+    # newfile = re.findall(regexpsearch, oldname)[0]
+    return newfile[0] + ".mp4"
+
+
+def convert(filename):
+    # print(filename)
+    cmd = cmdtempl
+    newfile = newname(filename)
+    if "ffmpeg" == cmd[0]:
+        cmd[2] = filename
+    else:
+        cmd[-3] = filename
+    cmd[-1] = newfile
+    # cmd = "{coder} -i '{src}' -codec copy -map 0:v -map 0:a -f mp4 '{dst}'"\
+        # .format(coder=ffmpeg, src=filename, dst=newfile)
+    # print(cmd)
+    proc = SP.Popen(cmd, stdout=SP.PIPE, stderr=SP.PIPE)
+    print('Converting "%s" to "%s"' % (filename, newfile))
+    res, err = proc.communicate()
+    if streamAnalyze(newfile):
+        return True
+    else:
+        return False
+
+
+def findfile(indir, srclist):
+    cmd = "find %s -name *.%s -print" % (indir, inext)
+    proc = SP.Popen(cmd.split(), stdout=SP.PIPE, stderr=SP.PIPE)
+    res, err = proc.communicate()
+    filelist = open(srclist, 'w')
+    filelist.write(res)
+    filelist.close()
+
+
+def runTest(filename):
+    """Running test media file"""
+    cmd = cmdffp
+    isfile = os.access(filename, os.F_OK)
+    if isfile:
+        cmd[-1] = '%s' % filename
+        proc = SP.Popen(cmd, stdout=SP.PIPE, stderr=SP.PIPE)
+        ffpout, ffperr = proc.communicate()
+        return ffpout
+    else:
+        return '{}'
+
+
+def streamAnalyze(filename):
+    """Simple stream analyzer"""
+    jarray = runTest(filename)
+    d = json.loads(jarray)
+    msg = False
+    try:
+        array = d['streams']
+        if len(array) > 1:
+            msg = True
+    except KeyError:
+        msg = False
+    return msg
+
+
+def main():
+    try:
+        indir = sys.argv[1]
+        try:
+            srclist = sys.argv[2]
+        except KeyError:
+            print('Using as file list %s' % srclist)
+    except KeyError:
+        print('Using as inputdir %s file list %s' % (indir,srclist))
+    findfile(indir, srclist)
+    infile = open(srclist, 'r')
+    insrc = infile.readlines()
+    infile.close()
+    for inp in insrc:
+        log_file = open(logfile, 'a')
+        filename = inp.strip('\n')
+        srccheck = streamAnalyze(newname(filename))
+        if srccheck:
+            print('%s:"%s" Was converted\n' % (str(logging.time.time()), filename))
+            log_file.write('%s:"%s" Was converted\n' % (str(logging.time.ctime()), filename))
+            # try:
+            #     os.unlink(filename)
+            # except OSError:
+            #     log_file.write('%s:"%s" OSError!\n' % (str(logging.time.ctime()), filename))
+        else:
+            print('"%s" Will convert' % filename)
+            res = convert(filename)
+            if res:
+                check = streamAnalyze(newname(filename))
+                if check:
+                    log_file.write('%s:"%s" Success\n' % (str(logging.time.ctime()), filename))
+                    # os.unlink(filename)
+                    log_file.write('%s:"%s" Deleted\n' % (str(logging.time.ctime()), filename))
+                else:
+                    log_file.write('%s:"%s" Failed\n' % (str(logging.time.ctime()), filename))
+            else:
+                log_file.write('%s:"%s" Failed\n' % (str(logging.time.ctime()), filename))
+        log_file.close()
+
+
+if __name__ == '__main__':
+    main()
